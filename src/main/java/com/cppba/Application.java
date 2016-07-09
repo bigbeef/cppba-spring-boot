0497584ff210325e183751c1587d3adbb0a7227b
245655812@qq.com <a19921121>
2016-07-10 01:08:36 +0800

    微改

--- src/main/java/com/cppba/Application.java
+++ src/main/java/com/cppba/Application.java
@@ -1,6 +1,5 @@
 package com.cppba;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
--- src/main/java/com/cppba/web/CommonAction.java
+++ src/main/java/com/cppba/web/CommonAction.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -18,10 +20,18 @@ public class CommonAction {
     private SessionFactory sessionFactory;
 
     @RequestMapping("test")
-    public void test(){
+    public void test(HttpServletResponse response){
         Session session = sessionFactory.getCurrentSession();
         SQLQuery sqlQuery = session.createSQLQuery("select * from user");
         List list = sqlQuery.list();
         System.out.printf(list.size()+"");
+        try {
+            response.setContentType("application/json");
+            response.setHeader("Cache-Control", "no-cache");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write("{\"msg\":\"调用成功\"}");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
