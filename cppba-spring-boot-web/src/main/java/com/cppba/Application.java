8246f5cb757b6679a5ce7fea21345308cb6a15cd
zwf <245655812@qq.com>
2017-05-19 23:24:04 +0800

    spring data jpa

--- cppba-spring-boot-web/src/main/java/com/cppba/Application.java
+++ cppba-spring-boot-web/src/main/java/com/cppba/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories/*(repositoryFactoryBeanClass = BaseRepositoryFactoryBean.class)*/
+@EnableJpaRepositories
 // same as @Configuration @EnableAutoConfiguration @ComponentScan
 @SpringBootApplication
 //WebFilter,WebServlet,WebListener
